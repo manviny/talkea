89ae75dcf93578e59f29d3917f901f98e1aa43a0
manviny <manol@mac.home>
2014-06-13 08:19:01 +0200

    added ngCordova $cordovaGeolocation

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,9 +2,10 @@
 angular.module('Talkea.controllers', [])
 
 .controller('AppCtrl', function($scope) {
+
 })
 
-.controller('PlaylistsCtrl', function($scope) {
+.controller('PlaylistsCtrl', function($scope, $cordovaGeolocation) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
     { title: 'Chill', id: 2 },
@@ -13,6 +14,21 @@ angular.module('Talkea.controllers', [])
     { title: 'Rap', id: 5 },
     { title: 'Cowbell', id: 6 }
   ];
+  
+    $cordovaGeolocation.getCurrentPosition().then(function(position) {
+      // Position here: position.coords.latitude, position.coords.longitude
+    }, function(err) {
+      // error
+    });s
+
+  $cordovaGeolocation.watchPosition().then(function() {
+      // Not currently used
+    }, function(err) {
+      // An error occured. Show a message to the user
+    }, function(position) {
+      // Active updates of the position here
+     console.log(position)
+  });
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
